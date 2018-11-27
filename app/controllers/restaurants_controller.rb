a14daeca30a06ecc62f74f35932889fc36341ce9
Côme Delaval <come@mbp-de-come.home>
2018-11-27 16:29:37 +0100

    create a search field for filter the restaurants and create the method to find the results

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -2,6 +2,9 @@ class RestaurantsController < ApplicationController
 
   def index
     @restaurants = Restaurant.order("rating desc")
+    if params[:search]
+      @restaurants = @restaurants.where("name LIKE ? OR address LIKE ? ", "%#{params[:search]}%", "%#{params[:search]}%")
+    end
   end
 
 end
