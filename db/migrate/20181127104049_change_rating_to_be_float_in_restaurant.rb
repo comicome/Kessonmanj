d88a46d63b148cbef7cfb142098a18b53d865bce
Côme Delaval <come@mbp-de-come.home>
2018-11-27 12:27:48 +0100

    create migrations to have equivalent table schema to the json

---
+++ db/migrate/20181127104049_change_rating_to_be_float_in_restaurant.rb
@@ -0,0 +1,9 @@
+class ChangeRatingToBeFloatInRestaurant < ActiveRecord::Migration[5.2]
+  def up
+    change_column :restaurants, :rating, :float
+  end
+
+  def down
+    change_column :restaurants, :rating, :integer
+  end
+end
