a6b2c4ea76a9f74ba85f681c5ddd1176420382da
Côme Delaval <come@mbp-de-come.home>
2018-11-28 18:34:00 +0100

    add a show page and create a search feature

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -3,8 +3,12 @@ class RestaurantsController < ApplicationController
   def index
     @restaurants = Restaurant.order("rating desc")
     if params[:search]
-      @restaurants = @restaurants.where("name LIKE ? OR address LIKE ? ", "%#{params[:search]}%", "%#{params[:search]}%")
+      @restaurants = @restaurants.where("name LIKE ? OR address LIKE ?", "%#{params[:search]}%", "%#{params[:search]}%")
     end
+  end
+
+  def show
+    @restaurant = Restaurant.find(params[:id])
   end
 
 end
