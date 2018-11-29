c7ec44d443b8cb885e28d65a776354c62cbeb3c2
Côme Delaval <come@mbp-de-come.home>
2018-11-29 11:26:10 +0100

    create label form to evaluate restaurant

---
+++ app/controllers/label_controller.rb
@@ -0,0 +1,35 @@
+class LabelsController < ApplicationController
+
+  before_action :set_restaurant, only: [:create, :update, :destroy]
+
+  def create
+    @label = @restaurant.labels.new(label_params)
+    @label.user_id = current_user.id
+    if @label.save
+      redirect_to restaurant_path(@restaurant)
+    else
+      @errors = @label.errors.full_messages
+      render 'restaurants/show'
+    end
+  end
+
+  def update 
+
+  def destroy
+    @label = @restaurant.labels.find(params[:id])
+    if current_user.id == @label.user_id
+      @label.destroy
+      redirect_to restaurant_path(@restaurant)
+    end
+  end
+
+  private
+
+  def set_restaurant
+    @restaurant = Restaurant.find(params[:restaurant_id])
+  end
+
+  def label_params
+    params.require(:label).permit(:body)
+  end
+end
