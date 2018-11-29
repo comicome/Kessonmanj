9e01b6f0b9549761ce134590399985d2ec8b97ab
Côme Delaval <come@mbp-de-come.home>
2018-11-29 16:16:24 +0100

    add validates for label

--- app/models/label.rb
+++ app/models/label.rb
@@ -2,5 +2,5 @@ class Label < ApplicationRecord
   belongs_to :restaurant
   belongs_to :user
   validates :name, :user_id, :restaurant_id, presence: true 
-  validates :name, inclusion: { in: %w(Hônnete Surcoté)}
+  validates :name, inclusion: { in: %w(Honnête Surcoté)}
 end
