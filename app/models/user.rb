72692e30e0a172803bc2e073563cf40a8fa3db22
Côme Delaval <come@mbp-de-come.home>
2018-12-03 14:46:32 +0100

    add validator for user's email

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,5 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
   has_many :comments, dependent: :destroy
   has_many :labels, dependent: :destroy
+  validates :email, format: { with: URI::MailTo::EMAIL_REGEXP }, uniqueness: true
 end
