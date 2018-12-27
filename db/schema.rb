2480afda2a53aa7c86cfdb5ca72fcad1a6e8d5c4
Côme Delaval <come@mbp-de-come.home>
2018-12-27 09:59:32 +0100

    add longitude and latitude for restaurants

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_12_21_145035) do
+ActiveRecord::Schema.define(version: 2018_12_26_145152) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -65,6 +65,8 @@ ActiveRecord::Schema.define(version: 2018_12_21_145035) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "image_url"
+    t.float "latitude"
+    t.float "longitude"
   end
 
   create_table "taggings", id: :serial, force: :cascade do |t|
